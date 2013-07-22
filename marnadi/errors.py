7a11b35824d810aae5d87350961a64f09ce5822d
renskiy <srenskiy@gmail.com>
2013-07-22 11:25:48 +0400

    default headers now are getting from class property in HttpError

--- marnadi/errors.py
+++ marnadi/errors.py
@@ -9,20 +9,22 @@ HTTP_501_NOT_IMPLEMENTED = '501 Not Implemented'
 
 class HttpError(Exception):
 
+    default_headers = (
+        ('Content-Type', 'text/plain'),
+        ('Content-Length', 0),
+    )
+
     @property
     def headers(self):
-        default_headers = {
-            'Content-Type': 'text/plain',
-            'Content-Length': None,
-        }
+        default_headers = dict(self.default_headers)
         for header, value in self._headers:
             header, value = str(header).title(), str(value)
             if header in default_headers:
                 default_headers[header] = value
             else:
-                yield (header, value)
-        if default_headers['Content-Length'] is None:
-            default_headers['Content-Length'] = len(self.data)
+                yield header, value
+        default_headers['Content-Length'] = default_headers['Content-Length'] \
+            or len(self.data)
         for header in default_headers.iteritems():
             yield header
 
