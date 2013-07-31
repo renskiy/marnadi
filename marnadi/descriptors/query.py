261e05f828c2b257f36d34e6b04106d23f19a409
renskiy <srenskiy@gmail.com>
2013-07-31 08:55:13 +0400

    Query descriptor

--- marnadi/descriptors/query.py
+++ marnadi/descriptors/query.py
@@ -1,7 +1,34 @@
+import urlparse
+
 from marnadi.descriptors import Descriptor
 
 
 class Query(Descriptor):
-    # TODO finish implementation
 
-    pass
+    def __init__(self, **kwargs):
+        super(Query, self).__init__(**kwargs)
+        self._query = None
+
+    def __getitem__(self, query_param):
+        value = self.get(query_param)
+        if value is None:
+            raise KeyError
+        return value
+
+    @property
+    def query(self):
+        if self._query is None:
+            try:
+                self._query = urlparse.parse_qs(self.environ.query_string,
+                                                keep_blank_values=True)
+            except AttributeError:
+                self._query = {}
+        return self._query
+
+    def clone(self, owner_instance):
+        instance = super(Query, self).clone(owner_instance)
+        instance._query = None
+        return instance
+
+    def get(self, query_param, default=None):
+        return self.query.get(query_param, default)
