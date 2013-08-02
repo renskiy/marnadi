747cd95c406d5ee6eb291730ce884fce708ac312
renskiy <srenskiy@gmail.com>
2013-08-02 11:06:55 +0400

    The idea with mime decoders for Data descriptor

--- marnadi/descriptors/data.py
+++ marnadi/descriptors/data.py
@@ -4,4 +4,13 @@ from marnadi.descriptors import Descriptor
 class Data(Descriptor):
     # TODO finish implementation
 
-    pass
+    content_decoders = {
+        'multipart/form-data': 'marnadi.mime.multipart.form_data',
+        'application/json': 'marnadi.mime.application.json',
+        'application/x-www-form-urlencoded':
+            'marnadi.mime.application.x_www_form_urlencoded',
+    }
+
+    def __init__(self, *content_decoders, **kwargs):
+        super(Data, self).__init__(**kwargs)
+        self.content_decoders.update(content_decoders)
