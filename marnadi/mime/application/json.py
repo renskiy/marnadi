38a23e44c1a4e3032de088adf8584b399e679c1d
renskiy <srenskiy@gmail.com>
2013-08-08 14:19:33 +0400

    json decoder

--- marnadi/mime/application/json.py
+++ marnadi/mime/application/json.py
@@ -1,7 +1,10 @@
+import importlib
+json = importlib.import_module('json')  # workaround for importing global json
+
 from marnadi import mime
 
 
 class Decoder(mime.Decoder):
-    #TODO finish implementation
 
-    pass
+    def __call__(self, data, headers, content_params):
+        return json.loads(data)
