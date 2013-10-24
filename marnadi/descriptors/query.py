db48e616b90645ebf3ddb50ce4c3d4b797e0af40
renskiy <srenskiy@gmail.com>
2013-10-24 23:36:56 +0400

    optimized Query descriptor

--- marnadi/descriptors/query.py
+++ marnadi/descriptors/query.py
@@ -5,30 +5,15 @@ from marnadi.descriptors import Descriptor
 
 class Query(Descriptor):
 
-    def __init__(self):
-        super(Query, self).__init__()
-        self._query = None
-
     def clone(self, handler):
-        clone = super(Query, self).clone(handler)
-        clone._query = None
-        return clone
+        return self.decode(handler.environ)
 
-    def __contains__(self, name):
-        return name in self.query
-
-    def __getitem__(self, key):
-        return self.query[key]
-
-    @property
-    def query(self):
-        if self._query is None:
-            try:
-                self._query = urlparse.parse_qs(self.environ.query_string,
-                                                keep_blank_values=True)
-            except AttributeError:
-                self._query = {}
-        return self._query
-
-    def get(self, query_param, default=None):
-        return self.query.get(query_param, default)
+    @staticmethod
+    def decode(environ):
+        try:
+            return urlparse.parse_qs(
+                environ.query_string,
+                keep_blank_values=True,
+            )
+        except AttributeError:  # query_string not in environ
+            return {}
