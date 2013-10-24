c8c8f6c267ae2327df83b1372aa9eecbe49603f1
renskiy <srenskiy@gmail.com>
2013-10-24 23:22:13 +0400

    optimized Data descriptor

--- marnadi/descriptors/data.py
+++ marnadi/descriptors/data.py
@@ -9,37 +9,17 @@ class Data(Descriptor):
     def __init__(self, *content_decoders):
         super(Data, self).__init__()
         self.content_decoders = dict(content_decoders)
-        self.headers = None
-        self._body = None
 
     def clone(self, owner_instance):
-        clone = super(Data, self).clone(owner_instance)
-        clone.content_decoders = self.content_decoders
-        clone.headers = owner_instance.headers
-        clone._body = None
-        return clone
+        return self.decode(
+            owner_instance.environ['wsgi.input'],
+            owner_instance.headers,
+        )
 
-    def __contains__(self, name):
-        return name in self.body
-
-    def __getitem__(self, key):
-        return self.body[key]
-
-    @property
-    def body(self):
-        if self._body is None:
-            self._body = self.decode()
-        return self._body
-
-    def get(self, name, default=None):
-        if isinstance(self.body, dict):
-            return self.body.get(name, default)
-        return default
-
-    def decode(self):
-        content_type, content_params = self.headers.get_splitted('Content-Type')
+    def decode(self, stream, headers):
+        content_type, content_params = headers.get_splitted('Content-Type')
         decode = self.get_decoder(content_type)
-        return decode(self.environ['wsgi.input'], self.headers, content_params)
+        return decode(stream, headers, content_params)
 
     def get_decoder(self, content_type):
         if content_type not in self.content_decoders:
