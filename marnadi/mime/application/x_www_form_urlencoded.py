ae803ebfa565bc4ceb4e55b8f78a41085da94960
renskiy <srenskiy@gmail.com>
2013-10-26 09:46:02 +0400

    x-www-form-urlencoded decoder returns pairs of name and value instead of dict

--- marnadi/mime/application/x_www_form_urlencoded.py
+++ marnadi/mime/application/x_www_form_urlencoded.py
@@ -6,7 +6,7 @@ from marnadi import mime
 class Decoder(mime.Decoder):
 
     def __call__(self, stream, headers, content_params):
-        return urlparse.parse_qs(
+        return urlparse.parse_qsl(
             ''.join(stream),
             keep_blank_values=True,
         )
