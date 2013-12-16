393c9708e1583cd09758b05a383fc589266c0595
renskiy <srenskiy@gmail.com>
2013-12-16 17:06:02 +0400

    Tests of Environ class

---
+++ tests/test_wsgi.py
@@ -0,0 +1,32 @@
+import unittest
+
+from marnadi import wsgi
+
+
+class EnvironTestCase(unittest.TestCase):
+
+    def test_attribute_getter(self):
+        original_environ = {
+            'wsgi.input': 'input',
+            'CONTENT_TYPE': 'content_type',
+            'CONTENT_LENGTH': 'content_length',
+            'HTTP_COOKIES': 'cookies',
+        }
+        environ = wsgi.Environ(original_environ)
+
+        self.assertEqual(getattr(environ, 'wsgi.input'), 'input')
+        self.assertEqual(environ['wsgi.input'], 'input')
+
+        self.assertEqual(environ.http_content_type, 'content_type')
+        self.assertEqual(environ['CONTENT_TYPE'], 'content_type')
+
+        self.assertEqual(environ.http_content_length, 'content_length')
+        self.assertEqual(environ['CONTENT_LENGTH'], 'content_length')
+
+        self.assertEqual(environ.http_cookies, 'cookies')
+        self.assertEqual(environ['HTTP_COOKIES'], 'cookies')
+
+        with self.assertRaises(AttributeError):
+            getattr(environ, 'unknown_wsgi_key')
+        with self.assertRaises(KeyError):
+            environ['unknown_wsgi_key']
