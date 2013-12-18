675d867bc1ee6f02b3845074889f1d2b6db25ab2
renskiy <srenskiy@gmail.com>
2013-12-18 10:35:10 +0400

    protect all Lazy's properties

--- marnadi/__init__.py
+++ marnadi/__init__.py
@@ -4,13 +4,13 @@ import importlib
 
 class Lazy(object):
 
-    __slots__ = ('path', 'value', 'args', 'kwargs')
+    __slots__ = ('_path', '_value', '_args', '_kwargs')
 
     def __init__(self, path, *args, **kwargs):
-        self.path = path
-        self.value = None
-        self.args = args
-        self.kwargs = kwargs
+        self._path = path
+        self._value = None
+        self._args = args
+        self._kwargs = kwargs
 
     def __call__(self, *args, **kwargs):
         return self.obj(*args, **kwargs)
@@ -29,11 +29,11 @@ class Lazy(object):
 
     @property
     def obj(self):
-        if self.value is None:
-            module_name, obj_name = self.path.rsplit('.', 1)
+        if self._value is None:
+            module_name, obj_name = self._path.rsplit('.', 1)
             module = importlib.import_module(module_name)
             value = getattr(module, obj_name)
             if callable(value):
-                value = functools.partial(value, *self.args, **self.kwargs)
-            self.value = value
-        return self.value
+                value = functools.partial(value, *self._args, **self._kwargs)
+            self._value = value
+        return self._value
