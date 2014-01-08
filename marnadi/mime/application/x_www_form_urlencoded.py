90aad6ab6aa3faf3dbbf8f1ebb92cc4b3757211b
renskiy <srenskiy@gmail.com>
2014-01-08 12:24:05 +0400

    x-www-form-urlencoded decoder returns multiple-value dict instead of list

--- marnadi/mime/application/x_www_form_urlencoded.py
+++ marnadi/mime/application/x_www_form_urlencoded.py
@@ -6,7 +6,7 @@ from marnadi import mime
 class Decoder(mime.Decoder):
 
     def __call__(self, stream, headers):
-        return urlparse.parse_qsl(
+        return urlparse.parse_qs(
             ''.join(stream),
             keep_blank_values=True,
         )
