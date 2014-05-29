01bb5940c70700a6b94cd7c1a9577b27c2c3f978
Rinat Khabibiev <rinat@banki.ru>
2014-05-29 18:55:11 +0400

    cached_property: try to return cached value immediately

--- marnadi/utils.py
+++ marnadi/utils.py
@@ -80,8 +80,10 @@ class cached_property(object):
     def __get__(self, instance, instance_type=None):
         if self.get is None:
             raise AttributeError('unreadable attribute')
-        if self.get.__name__ in instance.__dict__:
+        try:
             return instance.__dict__[self.get.__name__]
+        except KeyError:
+            pass
         result = instance.__dict__[self.get.__name__] = self.get(instance)
         return result
 
