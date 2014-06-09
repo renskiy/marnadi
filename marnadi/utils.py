39a7bc16ed03d552c38d4eebe9cff4f65928552d
Rinat Khabibiev <rinat@banki.ru>
2014-06-09 17:08:07 +0400

    Lazy uses cached_property to store object value

--- marnadi/utils.py
+++ marnadi/utils.py
@@ -23,62 +23,6 @@ def metaclass(mcs):
     return _decorator
 
 
-class LazyType(type):
-
-    def __call__(cls, path):
-        if isinstance(path, cls):
-            return path
-        elif isinstance(path, str):
-            return super(LazyType, cls).__call__(path)
-        return path
-
-
-@metaclass(LazyType)
-class Lazy(object):
-
-    def __init__(self, path):
-        self._value = None
-        self._value_set = False
-        self._path = path
-
-    def __call__(self, *args, **kwargs):
-        return self._obj(*args, **kwargs)
-
-    def __iter__(self):
-        return self._obj.__iter__()
-
-    def __str__(self):
-        return str(self._obj)
-
-    def __unicode__(self):
-        return unicode(self._obj)
-
-    def __bytes__(self):
-        return bytes(self._obj)
-
-    def __getitem__(self, item):
-        return self._obj[item]
-
-    def __getattr__(self, attr):
-        return getattr(self._obj, attr)
-
-    @property
-    def __class__(self):
-        return self._obj.__class__
-
-    @property
-    def _obj(self):
-        if not self._value_set:
-            try:
-                module_name, obj_name = self._path.rsplit('.', 1)
-            except ValueError:
-                module_name, obj_name = self._path, None
-            module = importlib.import_module(module_name)
-            self._value = obj_name and getattr(module, obj_name) or module
-            self._value_set = True
-        return self._value
-
-
 class cached_property(object):
 
     def __init__(self, fget=None, fset=None, fdel=None, doc=None):
@@ -115,6 +59,59 @@ class cached_property(object):
 
     def deleter(self, deleter):
         self.delete = deleter
+
+
+class LazyType(type):
+
+    def __call__(cls, path):
+        if isinstance(path, cls):
+            return path
+        elif isinstance(path, str):
+            return super(LazyType, cls).__call__(path)
+        return path
+
+
+@metaclass(LazyType)
+class Lazy(object):
+
+    def __init__(self, path):
+        self._path = path
+
+    def __call__(self, *args, **kwargs):
+        return self._obj(*args, **kwargs)
+
+    def __iter__(self):
+        return self._obj.__iter__()
+
+    def __str__(self):
+        return str(self._obj)
+
+    def __unicode__(self):
+        return unicode(self._obj)
+
+    def __bytes__(self):
+        return bytes(self._obj)
+
+    def __getitem__(self, item):
+        return self._obj[item]
+
+    def __getattr__(self, attr):
+        return getattr(self._obj, attr)
+
+    @property
+    def __class__(self):
+        return self._obj.__class__
+
+    @cached_property
+    def _obj(self):
+        try:
+            module_name, obj_name = self._path.rsplit('.', 1)
+        except ValueError:
+            module_name, obj_name = self._path, None
+        module = importlib.import_module(module_name)
+        if obj_name is not None:
+            return getattr(module, obj_name)
+        return module
 
 
 def to_bytes(obj, encoding='utf-8', error_callback=None):
