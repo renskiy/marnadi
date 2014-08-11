06fe2d6620edfaf60fe0a04c9a316cad7fe9a5f7
Rinat Khabibiev <rinat@banki.ru>
2014-08-11 20:01:54 +0400

    check stream and encoding before use

--- marnadi/descriptors/data/decoders/application/json.py
+++ marnadi/descriptors/data/decoders/application/json.py
@@ -8,10 +8,16 @@ class Decoder(BaseDecoder):
 
     __slots__ = ('stream', 'encoding')
 
+    def __init__(self):
+        self.encoding = None
+        self.stream = None
+
     def __call__(self, request):
         self.encoding = request.content_type.params.get('charset', 'utf-8')
         self.stream = request.input
         return json.load(self)
 
     def read(self, *args, **kwargs):
+        assert self.encoding and self.stream, \
+            "Either stream or encoding is not provided"
         return self.stream.read(*args, **kwargs).decode(encoding=self.encoding)
