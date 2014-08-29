b76376400d5c763b6600ba51aa966a40547c68a6
Rinat Khabibiev <rinat@banki.ru>
2014-08-29 17:04:01 +0400

    next version (0.1.1)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as description:
 
 setup(
     name='Marnadi',
-    version='0.1.0',
+    version='0.1.1',
     author='Rinat Khabibiev',
     author_email='srenskiy@gmail.com',
     packages=[
