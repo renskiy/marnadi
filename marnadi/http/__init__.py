ab8f8d9ef36d75387b3e5f5b7d78e0ce3ba0f585
Rinat Khabibiev <srenskiy@gmail.com>
2015-06-15 08:34:04 +0300

    Method and Handler moved to http package

--- marnadi/http/__init__.py
+++ marnadi/http/__init__.py
@@ -1,4 +1,5 @@
 import collections
+import functools
 
 from marnadi.http.data import Data
 from marnadi.http.cookies import Cookies
@@ -51,3 +52,72 @@ class Header(collections.Mapping):
                 for attr_name, attr_value in self.params.items()
             ),
         )
+
+
+class Handler(type):
+
+    def __new__(mcs, name, mro, attributes):
+        for attribute, value in attributes.items():
+            if isinstance(value, Method):
+                value.name = attribute
+        return super(Handler, mcs).__new__(mcs, name, mro, attributes)
+
+    def start(cls, *args, **kwargs):
+        raise NotImplementedError
+
+
+class Method(object):
+
+    __slots__ = 'name', 'func'
+
+    class FunctionHandler(Handler):
+
+        __function__ = NotImplemented
+
+        __response__ = NotImplemented
+
+        def __call__(cls, *args, **kwargs):
+            return cls.__function__(*args, **kwargs)
+
+        class classmethod(classmethod):
+
+            def __get__(self, instance, instance_class):
+                assert isinstance(instance_class, Method.FunctionHandler)
+                return functools.partial(
+                    self.__func__, instance_class.__response__)
+
+    def __init__(self, func=None, name=None):
+        self.func = func
+        self.name = name or func and func.__name__
+
+    def __get__(self, response, response_class):
+        if response is None:
+            return functools.partial(self, response_class)
+        return self.func and functools.partial(self.func, response)
+
+    def __call__(self, response_class, callback):
+        method = staticmethod(callback)
+        if isinstance(callback, self.FunctionHandler):
+            setattr(callback.__response__, self.name, method)
+            return callback
+        attributes = dict(
+            __module__=callback.__module__,
+            __doc__=callback.__doc__,
+            __slots__=(),
+        )
+        response = type(callback.__name__, (response_class, ), dict(
+            {self.name: method},
+            **attributes
+        ))
+        callback_replacement = self.FunctionHandler(
+            callback.__name__,
+            (),
+            dict(
+                attributes,
+                __function__=method,
+                __response__=response,
+                start=self.FunctionHandler.classmethod(
+                    response_class.start.__func__),
+            ),
+        )
+        return callback_replacement
--- marnadi/response.py
+++ marnadi/response.py
@@ -1,5 +1,4 @@
 import collections
-import functools
 import itertools
 import logging
 
@@ -13,76 +12,7 @@ except NameError:
     pass
 
 
-class Handler(type):
-
-    def __new__(mcs, name, mro, attributes):
-        for attribute, value in attributes.items():
-            if isinstance(value, Method):
-                value.name = attribute
-        return super(Handler, mcs).__new__(mcs, name, mro, attributes)
-
-    def start(cls, *args, **kwargs):
-        raise NotImplementedError
-
-
-class Method(object):
-
-    __slots__ = 'name', 'func'
-
-    class FunctionHandler(Handler):
-
-        __function__ = NotImplemented
-
-        __response__ = NotImplemented
-
-        def __call__(cls, *args, **kwargs):
-            return cls.__function__(*args, **kwargs)
-
-        class classmethod(classmethod):
-
-            def __get__(self, instance, instance_class):
-                assert isinstance(instance_class, Method.FunctionHandler)
-                return functools.partial(
-                    self.__func__, instance_class.__response__)
-
-    def __init__(self, func=None, name=None):
-        self.func = func
-        self.name = name or func and func.__name__
-
-    def __get__(self, response, response_class):
-        if response is None:
-            return functools.partial(self, response_class)
-        return self.func and functools.partial(self.func, response)
-
-    def __call__(self, response_class, callback):
-        method = staticmethod(callback)
-        if isinstance(callback, self.FunctionHandler):
-            setattr(callback.__response__, self.name, method)
-            return callback
-        attributes = dict(
-            __module__=callback.__module__,
-            __doc__=callback.__doc__,
-            __slots__=(),
-        )
-        response = type(callback.__name__, (response_class, ), dict(
-            {self.name: method},
-            **attributes
-        ))
-        callback_replacement = self.FunctionHandler(
-            callback.__name__,
-            (),
-            dict(
-                attributes,
-                __function__=method,
-                __response__=response,
-                start=self.FunctionHandler.classmethod(
-                    response_class.start.__func__),
-            ),
-        )
-        return callback_replacement
-
-
-@metaclass(Handler)
+@metaclass(http.Handler)
 class Response(object):
 
     logger = logging.getLogger('marnadi')
@@ -171,18 +101,18 @@ class Response(object):
             if getattr(self, method.lower()):
                 yield method
 
-    @Method
+    @http.Method
     def options(self, **kwargs):
         self.headers['Allow'] = ', '.join(self.allowed_http_methods)
 
-    get = Method()
+    get = http.Method()
 
-    head = Method()
+    head = http.Method()
 
-    post = Method()
+    post = http.Method()
 
-    put = Method()
+    put = http.Method()
 
-    patch = Method()
+    patch = http.Method()
 
-    delete = Method()
+    delete = http.Method()
